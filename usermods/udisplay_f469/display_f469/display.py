bb1f2d98752d1a7690a896f0e2504e12ea7f4876
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-01-17 14:13:44 +0100

    add autoupdate flag in display.init()

--- usermods/udisplay_f469/display_f469/display.py
+++ usermods/udisplay_f469/display_f469/display.py
@@ -1,16 +1,18 @@
 from udisplay import update
 
-def init():
+def init(autoupdate=True):
     import udisplay
-    import micropython
-    import pyb
-    def schedule(t):
-        """Try to schedule an LED update"""
-        try:
-            micropython.schedule(udisplay.update, 30)
-        except:
-            pass
     udisplay.init()
-    timer = pyb.Timer(4) # timer 4
-    timer.init(freq=30)  # 30Hz update rate
-    timer.callback(schedule)
+
+    if autoupdate:
+        import micropython
+        import pyb
+        def schedule(t):
+            """Try to schedule an LED update"""
+            try:
+                micropython.schedule(udisplay.update, 30)
+            except:
+                pass
+        timer = pyb.Timer(4) # timer 4
+        timer.init(freq=30)  # 30Hz update rate
+        timer.callback(schedule)
