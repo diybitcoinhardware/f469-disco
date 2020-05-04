b47151352d2f0fa4febc8589b8ad5cdcd3e5175f
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-05-04 04:29:15 +0200

    remove old secp

--- usermods/usecp256k1/README.md
+++
@@ -1,7 +0,0 @@
-# Micropython bindings for secp256k1 library
-
-## TODO:
-
-- make it compile with `-Wall` flag
-- add more functions via `#define USE_SECP256K1_something` (recovery, ecdh, schnorr)
-- write a usage example
--- usermods/usecp256k1/micropython.mk
+++
@@ -1,9 +0,0 @@
-BITCOIN_MOD_DIR := $(USERMOD_DIR)
-
-# Add all C files to SRC_USERMOD.
-SRC_USERMOD += $(BITCOIN_MOD_DIR)/secp256k1/src/secp256k1.c
-SRC_USERMOD += $(BITCOIN_MOD_DIR)/config/ext_callbacks.c
-SRC_USERMOD += $(BITCOIN_MOD_DIR)/libsecp256k1.c
-
-# We can add our module folder to include paths if needed
-CFLAGS_USERMOD += -I$(BITCOIN_MOD_DIR)/secp256k1 -I$(BITCOIN_MOD_DIR)/secp256k1/src -I$(BITCOIN_MOD_DIR)/config -DHAVE_CONFIG_H
