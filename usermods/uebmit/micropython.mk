28b262af49c61882048a217b7ca6ace729848f41
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-05-13 18:17:09 +0200

    store words in flash

---
+++ usermods/uebmit/micropython.mk
@@ -0,0 +1,9 @@
+UEMBIT_MOD_DIR := $(USERMOD_DIR)
+
+# Add all C files to SRC_USERMOD.
+SRC_USERMOD += $(UEMBIT_MOD_DIR)/uembit.c
+SRC_USERMOD += $(UEMBIT_MOD_DIR)/src/wordlist_bip39.c
+SRC_USERMOD += $(UEMBIT_MOD_DIR)/src/wordlist_slip39.c
+
+# We can add our module folder to include paths if needed
+CFLAGS_USERMOD += -I$(UEMBIT_MOD_DIR)/include -DMODULE_UEMBIT_ENABLED=1
