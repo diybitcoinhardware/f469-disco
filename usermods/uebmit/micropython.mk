243d6652755ca5da46c323a9597b37b884e4314c
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-05-25 17:00:55 +0200

    remove c-wordlist impl

--- usermods/uebmit/micropython.mk
+++ usermods/uebmit/micropython.mk
@@ -6,4 +6,5 @@ SRC_USERMOD += $(UEMBIT_MOD_DIR)/src/wordlist_bip39.c
 SRC_USERMOD += $(UEMBIT_MOD_DIR)/src/wordlist_slip39.c
 
 # We can add our module folder to include paths if needed
-CFLAGS_USERMOD += -I$(UEMBIT_MOD_DIR)/include -DMODULE_UEMBIT_ENABLED=1
+# disable for now, more optimizations required for faster word lookups
+CFLAGS_USERMOD += -I$(UEMBIT_MOD_DIR)/include -DMODULE_UEMBIT_ENABLED=0
