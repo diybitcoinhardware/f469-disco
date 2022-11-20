6e72b3e6985176e7d1ff3dda71221a36d28b5233
Stepan Snigirev <snigirev.stepan@gmail.com>
2022-11-20 13:18:32 +0100

    update character mapping to be less confusing

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     lv.style_copy(style, lbl.get_style(0))
     style.text.font = lv.font_custom_symbols_16
     lbl.set_style(0, style)
-    lbl.set_text('123456789')
+    lbl.set_text('0123456789')
 
     while True:
         time.sleep_ms(30)
